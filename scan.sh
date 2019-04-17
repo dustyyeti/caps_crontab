6e07e755ade0a54b1901bf586f665a0ce4a4e79f
conrad <mear8979@vandals.uidaho.edu>
2019-04-17 13:05:09 -0700

    fixing error in scan, other minute things

--- scan.sh
+++ scan.sh
@@ -14,14 +14,13 @@ fi
 
 EXPERIMENT_BASENAME=${LOCAL_DIR##*/}
 SCAN_FILENAME=$EXPERIMENT_BASENAME_R$RESOLUTION
-#SCANNER_LIST=$(scanimage -f "%d%n")
+SCANNER_LIST=$(scanimage -f "%d%n")
 
 date
 echo "Scanners:"
 echo "$SCANNER_LIST"
 echo "Experiment \"$EXPERIMENT_BASENAME\" will be stored in $LOCAL_DIR"
 echo "----------------------------------------------------------------"
-
 for scanner in $SCANNER_LIST; do
     scanner_safename=${scanner//:/_}
     FILENAME=$SCAN_FILENAME_$scanner_safename.R$RESOLUTION.$ENUM.tiff
@@ -29,8 +28,7 @@ for scanner in $SCANNER_LIST; do
     echo "Scanning on $scanner"
     echo "Saving as $FILENAME"
 
-    scanimage -d $scanner --resolution $RESOLUTION --mode Color --format tiff >
-$LOCAL_DIR/$FILENAME
+    scanimage -d $scanner --resolution $RESOLUTION --mode Color --format tiff > $LOCAL_DIR/$FILENAME
 done
 
 echo $ENUM > $LOCAL_DIR/count
