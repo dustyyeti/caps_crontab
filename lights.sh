b2d5a45a94713c6261fdc08921a9f6e3cc33f9f8
conrad <mear8979@vandals.uidaho.edu>
2019-05-14 09:03:45 -0700

    Update files for a specific experiment

--- lights.sh
+++ lights.sh
@@ -12,18 +12,12 @@ R="#FF0000"
 G="#00FF00"
 B="#0000FF"
 
-LED[0]="$OFF"
-LED[1]="$R"
-LED[2]="$R"
-LED[3]="#F0F0FF"
-LED[4]="#00FF00"
-LED[5]="#00FF00"
-LED[6]="#00FF00"
-LED[7]="#00FF00"
-LED[8]="#00FF00"
-LED[9]="#00FF00"
-LED[10]="#00FF00"
-LED[11]="#00FF00"
+LED[0]="$B"
+LED[1]="$B"
+LED[2]="$B"
+LED[3]="$B"
+LED[4]="$B"
+
 
 if [ "$OPTION" == "on" ]; then
 	for i in ${!LED[@]}; do
