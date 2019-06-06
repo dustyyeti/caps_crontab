9dcf39510dae470a84a923cc95bd0a8ac6b4b6f2
dustyyeti <yeti.shyn@gmail.com>
2019-06-05 23:48:57 -0700

    prior to variable format rewrite

--- go.sh
+++ go.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SP="/home/caps/scripts/caps_cronscan"
+
 ## Color codes for UI
 #. Reset
 NC='\033[0m'       # Text Reset
@@ -33,10 +35,6 @@ UBlack='\033[4;30m'
 
 On_IBlack='\033[0;100m'
 
-BACK='\033[40m'
-UGreen='\033[4;32m'
-FORE='\033[1;92m'
-IWhite='\033[0;97m'
 Inv='\e[7m'
 
 BIWhite='\033[1;95m'
@@ -44,15 +42,7 @@ BIPurple='\033[1;95m'
 
 Italic='\033[3m'
 
-## DISK OPS
-#. load last experiment
-source ./exp/last.exp #: in one commad, loads all variables
-EXP=$(echo $EXP|tr -d '\n')
-$INT=$(echo $INT|tr -d '\n')
-
-SP="/home/caps/scripts/caps_cronscan"
-
-## declare vars
+### DECLARE ARRAYS
 
 declare -a ARGS
 declare -a BLURBS
@@ -70,11 +60,10 @@ ARGS+=("REF")
 ARGS+=("XFER")
 ARGS+=("DELAY")
 ARGS+=("LIGHTS")
-ARGS+=("SNAME1")
+# ARGS+=("scanner_ID1")
 # ARGS+=("UNK2")
 # ARGS+=("UNK3")
 # ARGS+=("UNK4")
-
 
 # TYPE+=("STRING")	#EXP
 # TYPE+=("INT")		#SCANNERS
@@ -85,7 +74,6 @@ ARGS+=("SNAME1")
 # TYPE+=("INT")		#DELAY
 # TYPE+=("TOGGLE")	#LIGHTS
 
-
 BLURBS+=("Experiment Name")
 BLURBS+=("Scanner Count")
 BLURBS+=("Scan Interval Time")
@@ -94,7 +82,7 @@ BLURBS+=("* REF scan every frame")
 BLURBS+=("* server file transfer")
 BLURBS+=("series scan delay")
 BLURBS+=("* use lights")
-BLURBS+=("something dish")
+# BLURBS+=("scanner1 ID")
 # BLURBS+=("something lights")
 # BLURBS+=("something lights")
 # BLURBS+=("something lights")
@@ -107,43 +95,58 @@ SUB+=("_exp")
 SUB+=("_exp")
 SUB+=("_exp")
 SUB+=("_exp")
-SUB+=("_dish")
-SUB+=("_lights")
-SUB+=("_lights")
-SUB+=("_lights")
+# SUB+=("_dish")
+# SUB+=("_lights")
+# SUB+=("_lights")
+# SUB+=("_lights")
 
+SUBBLURBS+=("${Inv}_____Experiment Parameters_____${NC} [${Red} WARNING${NC} | ${LtBlue}LAST EXP${NC} | ${Green}new value${NC} ]")
+SUBBLURBS+=("${On_IBlack}___________Dish Setup__________${NC}")
+SUBBLURBS+=("${BCyan}${Inv}____Neopixel Light Program_____${NC}")
 
-KEYS=(e s i r z x d l k q w a)
-lKeys=${#KEYS[@]} 
+KEYS=(e s i r z x d l k)
 
-DARGS+=("UNK")
-DBLURBS+=("something")
+lKeys=${#KEYS[@]} #: establish length of KEYS array
 
-DKEYS=(k)
-lDKeys=${#DKEYS[@]}
+declare -i sStart=8 #: scanner id arg starts at index 7
 
-# LTARGS+=("SCANNERS")
-# LTBLURBS+=("Scanner Count")
-# LTKEYS=(s)
-# lLtKeys=${#LTKEYS[@]}
-
-
-## flow booleans
+###. flow booleans
 STAY_TF=true
 MATCH_TF=false
 
-testit (){ #. $1 the key | $2 the array set prefix
-	blurb=$2BLURBS
-	echo check = $check
-	if [[ $1 = $key ]]
-	then
-		printf "%32s" "New ${!blurb[$i]} > "
+Insert(){
+    h='
+################## insert ########################
+# Usage:
+#   insert arr_name index element
+#
+#   Parameters:
+#       arr_name    : Name of the array variable
+#       index       : Index to insert at
+#       element     : Element to insert
+##################################################
+    '
+    [[ $1 = -h ]] && { echo "$h" >/dev/stderr; return 1; }
+    declare -n __arr__=$1   # reference to the array variable
+    i=$2                    # index to insert at
+    el="$3"                 # element to insert
+    # handle errors
+    [[ ! "$i" =~ ^[0-9]+$ ]] && { echo "E: insert: index must be a valid integer" >/dev/stderr; return 1; }
+    (( $1 < 0 )) && { echo "E: insert: index can not be negative" >/dev/stderr; return 1; }
+    # Now insert $el at $i
+    __arr__=("${__arr__[@]:0:$i}" "$el" "${__arr__[@]:$i}")
 
 
-	fi
 }
 
-eat_keys (){
+Spacer (){ #: helps with UI building
+	echo
+	echo -e ${SUBBLURBS[$isub]}
+	printf '.%.0s' {1..31} #....................
+	echo
+}
+
+EatKeys (){ #: digest user key inputs
 		if [[ $1 = $key ]]
 		then
 			echo
@@ -164,139 +167,155 @@ eat_keys (){
 		fi
 }
 
-SUBBLURBS+=("${Inv}_____Experiment Parameters_____${NC} [${Red} WARNING${NC} | ${LtBlue}LAST EXP${NC} | ${Green}new value${NC} ]")
-SUBBLURBS+=("${On_IBlack}___________Dish Setup__________${NC}")
-SUBBLURBS+=("${BCyan}${Inv}____Neopixel Light Program_____${NC}")
-
-spacer (){
-	echo
-	echo -e ${SUBBLURBS[$isub]}
-	printf '.%.0s' {1..31} #....................
-	echo
-}
-
+### use loop to setup initial colors
 for ((i=0;i<$lKeys;i++))
 do
 	COLS+=($LtBlue)
 	#declare ${KEYS[$i]}Col=$LtBlue
 done
+COLS[0]=$Red #: set exp name to warning color
 
-COLS[0]=$Red
+### DISK OPS
+#. load last experiment
+source ./exp/last.exp #: in one commad, loads all variables
+EXP=$(echo $EXP|tr -d '\n') #? what do these lines do??
+$INT=$(echo $INT|tr -d '\n')
 
 
-### main looop ----------------------
-
-while [ "$STAY_TF" = "true" ] 
+### Insert ARGS based on startup settings
+for ((i=0;i<SCANNERS;i++))
 do
-	clear
-	echo -e "${BPurple}"
-	printf " CREATE NEW CRONTAB EXPERIMENT "
-	echo
-	# boo=$(printf "%29s" "${BLURBS[$i]} [")
-
-	# echo "$boo"
-	isub=0
-	for ((i=0;i<lKeys;i++))
-	do
-		# echo sub $sub
-		# echo "SUB[i]" $SUB[$i]
-		if [[ $sub != ${SUB[$i]} ]]
-		then
-			sub=${SUB[$i]}
-			if ! [[ $sub = "_lights" && $LIGHTS = "off" ]]
-			then
-				spacer isub
-				((isub++))
-			else
-				break
-			fi
-		fi
-		printf "%29s" "${BLURBS[$i]} ["
-		echo -e ${Cyan}${KEYS[$i]}${NC}"] "${COLS[$i]}${!ARGS[$i]}${NC}
-	done
-	echo
-	printf '_%.0s' {1..31}
-	echo
-	printf "%27s" "set new parameters with ["
-	echo -e ${Cyan}${Italic}"key"${NC}"]" 
-	printf "%25s" "start program ["
-	echo -e ${Cyan}${Italic}"enter"${NC}"]" 
-
-	##! possible light routine
-	# if [[ $LIGHTS == "on" ]]
-	# then
-	# 	printf "%25s" "to program lights ["
-	# 	echo -e ${Cyan}${Italic}"enter"${NC}"]" 
-	# else
-	# 	printf "%25s" "start program ["
-	# 	echo -e ${Cyan}${Italic}"enter"${NC}"]" 
-	# fi
-
-	echo ""
-	printf "%32s" "choice > "
-	
-###: USER INPUT
-	read -n 1 key
-	[[ $key = "" ]] && STAY_TF="false" #- enter key
-
-	#: experiment parms list
-	for ((i=0;i<lKeys;i++))
-	do
-		eat_keys ${KEYS[$i]} #: send the key to check for hotkey
-	done
-# sleep 1 #@ this is for debug
-done #: END WHILE STAY_TF LOOP
-
-EROOT=${SP}/exp/
-EP=$EROOT${EXP}
-if [ ! -d "$EP" ]; then
-    mkdir -p $EP
-fi
-
-### write out $EXP.exp and last.exp record files
-echo "
-writing $EXP.exp:"
-
-echo "EXP=$EXP SCANNERS=$SCANNERS INT=$INT RES=$RES REF=$REF LIGHTS=$LIGHTS XFER=$XFER DELAY=$DELAY" 2>&1 | tee $EROOT/last.exp
-cp $EROOT/last.exp $EP/$EXP.exp
-
-echo 
-echo "working with Directory $EP"
-
-echo -n "# programatic crontab file generated for CAPS scanner control
-
-# " > $EP/xtab
-printf '.%.0s' {1..29} >> $EP/xtab
-echo >> $EP/xtab
-for ((i=0;i<lKeys;i++))
-do
-	echo -n "#" >> $EP/xtab
-	printf "%31s" "${BLURBS[$i]}: " >> $EP/xtab
-	echo ${!ARGS[$i]} >> $EP/xtab
+	y=$sStart
+	# echo $i
+	# Insert ARGS 8 'scanner_ID1'
+	 # (( num += x ))
+	Insert ARGS $y scanner_ID$i
 done
 
-echo "
-sp=$SP" >> $EP/xtab
-echo "ep=$EP" >> $EP/xtab
+echo "${ARGS[@]}"
 
-printf "
-*/$INT * * * * " >> $EP/xtab
+read
+clear
+main (){
+### main looop --------------------------------------------
+	while [ "$STAY_TF" = "true" ] 
+	do
+		clear
+		echo -e "${BPurple}"
+		printf " CREATE NEW CRONTAB EXPERIMENT "
+		echo
+		# boo=$(printf "%29s" "${BLURBS[$i]} [")
 
-[[ $REF > 0 ]] && \
+		# echo "$boo"
+		isub=0
+		for ((i=0;i<lKeys;i++))
+		do
+			# echo sub $sub
+			# echo "SUB[i]" $SUB[$i]
+			if [[ $sub != ${SUB[$i]} ]]
+			then
+				sub=${SUB[$i]}
+				if ! [[ $sub = "_lights" && $LIGHTS = "off" ]]
+				then
+					Spacer isub
+					((isub++))
+				else
+					break
+				fi
+			fi
+			printf "%29s" "${BLURBS[$i]} ["
+			echo -e ${Cyan}${KEYS[$i]}${NC}"] "${COLS[$i]}${!ARGS[$i]}${NC}
+		done
+		echo
+		printf '_%.0s' {1..31}
+		echo
+		printf "%27s" "set new parameters with ["
+		echo -e ${Cyan}${Italic}"key"${NC}"]" 
+		printf "%25s" "start program ["
+		echo -e ${Cyan}${Italic}"enter"${NC}"]" 
 
-printf "\$sp/scan.sh $REF \$ep $DELAY 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
-[[ $LIGHTS == "on" ]] && \
-printf "\$sp/lights.sh off 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
-printf "\$sp/scan.sh $RES \$ep $DELAY 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
-[[ $LIGHTS == "on" ]] && \
-printf "\$sp/lights.sh on 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
-[[ $XFER == "on" ]] && \
-printf "\$sp/transfer.sh \$ep 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
-echo >> $EP/xtab ###- blank line needed before EOF
-echo
-echo "xtab exported"
-echo
-echo "install crontab..."
-echo
+		##! possible light routine
+		# if [[ $LIGHTS == "on" ]]
+		# then
+		# 	printf "%25s" "to program lights ["
+		# 	echo -e ${Cyan}${Italic}"enter"${NC}"]" 
+		# else
+		# 	printf "%25s" "start program ["
+		# 	echo -e ${Cyan}${Italic}"enter"${NC}"]" 
+		# fi
 
-crontab $EP/xtab
+		echo ""
+		printf "%32s" "choice > "
+		
+	###: USER INPUT
+		read -n 1 key
+		[[ $key = "" ]] && STAY_TF="false" #- enter key
+
+		#: experiment parms list
+		for ((i=0;i<lKeys;i++))
+		do
+			EatKeys ${KEYS[$i]} #: send the key to check for hotkey
+		done
+	# sleep 1 #@ this is for debug
+	done #: END WHILE STAY_TF LOOP
+
+	EROOT=${SP}/exp/
+	EP=$EROOT${EXP}
+	if [ ! -d "$EP" ]; then
+	    mkdir -p $EP
+	fi
+
+	### write out $EXP.exp and last.exp record files
+	echo "
+	writing $EXP.exp:"
+	echo "#exp parameters" 2>&1 | tee $EROOT/last.exp
+	for arg in "${ARGS[@]}"
+	do
+	   echo "${arg}=${!arg}" >> $EROOT/last.exp
+	done
+
+	cp $EROOT/last.exp $EP/$EXP.exp
+
+	echo 
+	echo "working with Directory $EP"
+
+	echo -n "# programatic crontab file generated for CAPS scanner control
+
+	# " > $EP/xtab
+	printf '.%.0s' {1..29} >> $EP/xtab
+	echo >> $EP/xtab
+	for ((i=0;i<lKeys;i++))
+	do
+		echo -n "#" >> $EP/xtab
+		printf "%31s" "${BLURBS[$i]}: " >> $EP/xtab
+		echo ${!ARGS[$i]} >> $EP/xtab
+	done
+
+	echo "
+	sp=$SP" >> $EP/xtab
+	echo "ep=$EP" >> $EP/xtab
+
+	printf "
+	*/$INT * * * * " >> $EP/xtab
+
+	[[ $REF > 0 ]] && \
+
+	printf "\$sp/scan.sh $REF \$ep $DELAY 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
+	[[ $LIGHTS == "on" ]] && \
+	printf "\$sp/lights.sh off 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
+	printf "\$sp/scan.sh $RES \$ep $DELAY 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
+	[[ $LIGHTS == "on" ]] && \
+	printf "\$sp/lights.sh on 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
+	[[ $XFER == "on" ]] && \
+	printf "\$sp/transfer.sh \$ep 2>&1 | tee -a \$ep/LOG; " >> $EP/xtab
+	echo >> $EP/xtab ###- blank line needed before EOF
+	echo
+	echo "xtab exported"
+	echo
+	echo "install crontab..."
+	echo
+
+	crontab $EP/xtab
+}
+
+main "$@"
